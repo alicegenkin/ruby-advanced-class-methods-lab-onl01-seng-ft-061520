e417efd6d0219b1c15272f134dcfbd2b04596b75
Alice <agenkin1@gmail.com>
2020-06-28 02:19:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,10 @@ def self.new_by_name(song_name)
   song
 end
 
-
+def self.create_by_name(song_name)
+  song = self.new
+  song.name = song_name
+  @@all << song
 end
 
 #Song.new_by_name("Hello World")
